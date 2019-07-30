1e94a912f36ecf92f73f06ad3cf8984e7215e0e3
someotherkyle <kylemclellan@gmail.com>
2019-07-30 12:19:08 -0400

    added constructor and board grid

--- src/containers/BoardContainer.js
+++ src/containers/BoardContainer.js
@@ -1,11 +1,41 @@
 import React, {Component} from 'react'
+import TileContainer from './TileContainer'
 
 export default class BoardContainer extends Component {
+    constructor(){
+        super()
+        this.state = {
+            occupied: []
+        }
+    }
 
     render(){
         return(
-            <div className='wtfAmIDoing'>
-
+            <div className='gameBoard'>
+                <div className='row-0'>
+                    <div className='col-0'></div>
+                    <div className='col-1'></div>
+                    <div className='col-2'></div>
+                    <div className='col-3'></div>
+                </div>
+                <div className='row-1'>
+                    <div className='col-0'></div>
+                    <div className='col-1'></div>
+                    <div className='col-2'></div>
+                    <div className='col-3'></div>
+                </div>
+                <div className='row-2'>
+                    <div className='col-0'></div>
+                    <div className='col-1'></div>
+                    <div className='col-2'></div>
+                    <div className='col-3'></div>
+                </div>
+                <div className='row-3'>
+                    <div className='col-0'></div>
+                    <div className='col-1'></div>
+                    <div className='col-2'></div>
+                    <div className='col-3'></div>
+                </div>
             </div>
         )
     }
