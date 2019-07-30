0db43016d5f57613edba8ef6c0f322043675708a
someotherkyle <kylemclellan@gmail.com>
2019-07-30 18:41:20 -0400

    created manageTiles reducer

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import App from './App'
 // huh -> ?  import * as serviceWorker from './serviceWorker'
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
-//will need a reducer here 
+import manageTiles from './reducers/manageTiles'
 
-let store = createStore(/*reducer*/, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+let store = createStore(manageTiles, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
     document.getElementById('root')
-    )
+)
 
 
-serviceWorker.unregister()
+//serviceWorker.unregister()
 // ^ ? 
---
+++ src/reducers/manageTiles.js
@@ -0,0 +1,8 @@
+export default function manageTiles(state = {
+    occupied: []
+}, action){
+    switch(action.type){
+        default:
+            return state
+    }
+}
