bd296cc8c0bbedd5a0c9750edf75110cce7b0f3c
someotherkyle <kylemclellan@gmail.com>
2019-07-30 09:06:26 -0400

    separated board container and presentational component.

---
+++ src/containers/BoardContainer.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class BoardContainer extends Component {
+
+    render(){
+        return(
+            <div className='wtfAmIDoing'>
+
+            </div>
+        )
+    }
+}
