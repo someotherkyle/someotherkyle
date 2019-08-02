e78510f225dc876c29f3c9127c8a56de53f21710
someotherkyle <kylemclellan@gmail.com>
2019-08-02 11:09:17 -0400

    Began adding logic to tilecontainer

--- src/containers/TileContainer.js
+++ src/containers/TileContainer.js
@@ -1,15 +1,29 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux';
+import colors from '../colors'
+//import helper functions 
 
-export default class TileContainer extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            index: -1,
-            value: -1
-        }
+function roundedRect(ctx, x, y, width, height, radius) {
+    ctx.beginPath()
+    ctx.moveTo(x, y + radius)
+    ctx.lineTo(x, y + height - radius)
+    ctx.arcTo(x, y + height, x + radius, y + height, radius)
+    ctx.lineTo(x + width - radius, y + height)
+    ctx.arcTo(x + width, y + height, x + width, y + height-radius, radius)
+    ctx.lineTo(x + width, y + radius)
+    ctx.arcTo(x + width, y, x + width - radius, y, radius)
+    ctx.lineTo(x + radius, y)
+    ctx.arcTo(x, y, x, y + radius, radius)
+    ctx.fill()
+}
+
+class TileContainer extends Component {
+    componentDidMount = () => {
+        
     }
+    initializeIndex = (props) => {
+        let proposedIndex = Math.random() * Math.floor(16) //hardcoded for now, will need to update w/ various board sizes
 
-    initializeIndex = () => {
         //TODO: verify index availability 
         //set index to randomly generated, available index
         let availableIndex = -1 //change that <--
@@ -28,3 +42,9 @@ export default class TileContainer extends Component {
         })
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        occupied: state.occupied
+    }
+}
+export default connect(mapStateToProps)(TileContainer)
