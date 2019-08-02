975c7874db9a487b8ddb7510029a4740d1a2da05
someotherkyle <kylemclellan@gmail.com>
2019-08-02 18:42:15 -0400

    created separate canvas for tiles

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Board = () => {
     return(
-        <canvas id='board' height='500' width='500' />
+        <div id='board-div'>
+            <canvas id='board' height='500' width='500' />
+            <canvas id='tile-canvas' height='500' width='500' />
+        </div>
     )
 }
 
