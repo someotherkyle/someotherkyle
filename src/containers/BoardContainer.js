65bb7e825b25926f9859c8704bd223e8e9677722
someotherkyle <kylemclellan@gmail.com>
2019-08-02 09:44:38 -0400

    Rounded tile holders on board, removed unwanted CSS from index

--- src/containers/BoardContainer.js
+++ src/containers/BoardContainer.js
@@ -2,17 +2,17 @@ import React, {Component} from 'react'
 import Board from '../components/Board'
 //import TileContainer from './TileContainer'
 function roundedRect(ctx, x, y, width, height, radius) {
-    ctx.beginPath();
-    ctx.moveTo(x, y + radius);
-    ctx.lineTo(x, y + height - radius);
-    ctx.arcTo(x, y + height, x + radius, y + height, radius);
-    ctx.lineTo(x + width - radius, y + height);
-    ctx.arcTo(x + width, y + height, x + width, y + height-radius, radius);
-    ctx.lineTo(x + width, y + radius);
-    ctx.arcTo(x + width, y, x + width - radius, y, radius);
-    ctx.lineTo(x + radius, y);
-    ctx.arcTo(x, y, x, y + radius, radius);
-    ctx.fill();
+    ctx.beginPath()
+    ctx.moveTo(x, y + radius)
+    ctx.lineTo(x, y + height - radius)
+    ctx.arcTo(x, y + height, x + radius, y + height, radius)
+    ctx.lineTo(x + width - radius, y + height)
+    ctx.arcTo(x + width, y + height, x + width, y + height-radius, radius)
+    ctx.lineTo(x + width, y + radius)
+    ctx.arcTo(x + width, y, x + width - radius, y, radius)
+    ctx.lineTo(x + radius, y)
+    ctx.arcTo(x, y, x, y + radius, radius)
+    ctx.fill()
 }
 
 export default class BoardContainer extends Component {
@@ -35,9 +35,8 @@ export default class BoardContainer extends Component {
             let a = 12
             for (let i = 0; i <= 4; i++){
                 let b = 12
-                const c = 110
                 for (let j = 0; j <=4; j++){
-                    ctx.fillStyle = 'rgb(147, 161, 161)';
+                    ctx.fillStyle = 'rgb(131, 148, 150)'
                     roundedRect(ctx, a, b, 110, 110, 10)
                     b += 122
                 }
