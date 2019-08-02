b8c08f6bb8d4a4d851241e7e4f145e6cc2cc9f58
someotherkyle <kylemclellan@gmail.com>
2019-08-02 19:29:35 -0400

    duplicating colors in object to match names and uses

--- src/colors.js
+++ src/colors.js
@@ -2,10 +2,10 @@ const colors = {
     base03: "rgb(0, 43, 54)",
     base02: "rgb(7, 54, 66)",
     base01: "rgb(88, 110, 117)",
-    darkText: "rgb(101, 123, 131)",
-    emptyTileBackground: "rgb(131, 148, 150)",
+    base00: "rgb(101, 123, 131)",
+    base0: "rgb(131, 148, 150)",
     base1: "rgb(147, 161, 161)",
-    lightText: "rgb(238, 232, 213)",
+    base2: "rgb(238, 232, 213)",
     base3: "rgb(253, 246, 227)",
     yellow: "rgb(181, 137, 0)",
     orange: "rgb(203, 75, 22)",
@@ -14,6 +14,11 @@ const colors = {
     violet: "rgb(108, 113, 196)",
     blue: "rgb(38, 139, 210)",
     cyan: "rgb(42, 161, 152)",
-    green: "rgb(133, 153, 0)"
+    green: "rgb(133, 153, 0)",
+    darkText: "rgb(101, 123, 131)", //base00
+    lightText: "rgb(238, 232, 213)", //base2
+    boardBackground: "rgb(101, 123, 131)", //base00
+    emptyTileBackground: "rgb(131, 148, 150)", //base0
+    2: "rgb(238, 232, 213)", //base2
 }
 export default colors
