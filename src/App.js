e83c628f2f16fb926dfd561663e4bcebbd26f774
someotherkyle <kylemclellan@gmail.com>
2019-08-04 16:57:08 -0400

    converted board to component to allow access to state? not sure about this but it wasn't working and does now so there's that. consolidated CSS into index, removed app.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom'
-import './App.css'
 import NavBar from './components/NavBar'
 import BoardContainer from './containers/BoardContainer'
 function App() {
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-const Board = () => {
-    return(
-        <div id='board-div'>
-            <p>Score: {this.state.score}</p>
-            <canvas id='game-board' height='500' width='500' />
-            <canvas id='tile-canvas' height='500' width='500' />
-        </div>
-    )
+class Board extends Component {
+    render(){
+        return(
+            <div id='board-div'>
+                <p id='score-board'>Score: {this.props.score}</p>
+                <canvas id='game-board' height='500' width='500' />
+                <canvas id='tile-canvas' height='500' width='500' />
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = state => {
