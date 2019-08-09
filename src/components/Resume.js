672534aa589a35cfad437aa34561db4fa840b4cb
someotherkyle <kylemclellan@gmail.com>
2019-08-09 08:47:32 -0400

    slapped rezzy on webby

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,7 @@ export default class Resume extends Component {
   render(){
     return (
       <div className='text-div'>
-        <p>Resume will go here.</p>
+        <embed src="/resume.pdf" height="850" width="1100"/>
       </div>
     )
   }
