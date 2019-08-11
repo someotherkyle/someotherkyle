a84bc8035344fca6b0185cc48972252fa6dd6fc9
someotherkyle <kylemclellan@gmail.com>
2019-08-11 10:22:55 -0400

    ensured all external links open in new tabs

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,7 +5,7 @@ export default class Blog extends Component {
   render(){
     return(
       <div className='text-div'>
-        <a href="https://someotherkyle.github.io">Temporary blog hosted on Github</a>
+        <a rel='noopener noreferrer' href="https://someotherkyle.github.io">Temporary blog hosted on Github</a>
         <p>I am in the process of hosting my blog here. Check back soon!</p>
       </div>
     )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,7 +15,7 @@ const NavBar = () => {
             <NavLink to='/projects'>Projects</NavLink>
             <div className='dropdown-content'>
               <NavLink to='/play2048'>2048 Tribute</NavLink>
-              <a href='http://someotherkyle-activity-tracker.herokuapp.com' rel='noopener noreferrer' target="_blank">Activity Tracker</a>
+              <a rel='noopener noreferrer' href='http://someotherkyle-activity-tracker.herokuapp.com' target="_blank">Activity Tracker</a>
             </div>
           </div>
           <NavLink to='/comments'>Comments & Feedback</NavLink>
