c588b9570ab98107c0189a63b57f8d252d135570
someotherkyle <kylemclellan@gmail.com>
2019-08-11 10:22:55 -0400

    ensured all external links open in new tabs

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default class About extends Component {
         <div className='about-content'>
           <img src="/profile.png" alt="my mug" />
           <div className='text-div'>
-            <h1 id='name'>i'm kyle. i solve problems.</h1>
+            <h1 id='name'>my name is kyle. i solve problems.</h1>
             <p>I am a full stack web developer with an aptitude for solving unique and challenging problems.  Currently, I am focused on expanding my knowledge 
               and abilities in programming, website development and quality assurance. With experience in Ruby on Rails, JavaScript, React, 
               Redux and a military background, I bring strong skills in team-building and project management that help solve real-world 
