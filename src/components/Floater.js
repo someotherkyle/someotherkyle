bac5261064d1eb5462bc4089a2e293db8c562465
someotherkyle <kylemclellan@gmail.com>
2019-08-14 19:25:55 -0400

    began reworking CSS for mobile

--- src/components/Floater.js
+++ src/components/Floater.js
@@ -4,10 +4,13 @@ export default class Floater extends Component {
 
   render(){
     return(
-      <div id='floater'>
-        <a href="mailto:kyle@someotherkyle.com"><i className='material-icons'>email</i></a>
-        <a rel='noopener noreferrer' target="_blank" href="https://github.com/someotherkyle"><img src="github.png" alt='github octocat' /></a>
-        <a rel='noopener noreferrer' target="_blank" href="https://linkedin.com/in/someotherkyle"><img src="linkedin.png" alt='linked in logo' /></a>
+      <div id='floater-wrapper'>
+        <i className='material-icons'>more_vert</i>
+        <div id='floater'>
+          <a href="mailto:kyle@someotherkyle.com"><i className='material-icons'>email</i></a>
+          <a rel='noopener noreferrer' target="_blank" href="https://github.com/someotherkyle"><img src="github.png" alt='github octocat' /></a>
+          <a rel='noopener noreferrer' target="_blank" href="https://linkedin.com/in/someotherkyle"><img src="linkedin.png" alt='linked in logo' /></a>
+        </div>
       </div>
     )
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,21 +4,23 @@ import { NavLink } from 'react-router-dom'
 const NavBar = () => {
     return(
         <div className='navbar'>
-          <div className='dropdown'>
-            <NavLink to='/'>About</NavLink>
-            <div className='dropdown-content'>
-              <NavLink to='/resume'>Resume</NavLink>
+          <div className='navbar-links'>
+            <div className='dropdown'>
+              <NavLink to='/' activeClassName='active'>About</NavLink>
+              <div className='dropdown-content'>
+                <NavLink to='/resume' activeClassName='active'>Resume</NavLink>
+              </div>
             </div>
-          </div>
-          <NavLink to='/blog'>Blog</NavLink>
-          <div className='dropdown'>
-            <NavLink to='/projects'>Projects</NavLink>
-            <div className='dropdown-content'>
-              <NavLink to='/play2048'>2048 Tribute</NavLink>
-              <a rel='noopener noreferrer' href='http://someotherkyle-activity-tracker.herokuapp.com' target="_blank">Activity Tracker</a>
+            <NavLink to='/blog' activeClassName='active'>Blog</NavLink>
+            <div className='dropdown'>
+              <NavLink to='/projects' activeClassName='active'>Projects</NavLink>
+              <div className='dropdown-content'>
+                <NavLink to='/play2048' activeClassName='active'>2048 Tribute</NavLink>
+                <a rel='noopener noreferrer' href='http://someotherkyle-activity-tracker.herokuapp.com' target="_blank">Activity Tracker</a>
+              </div>
             </div>
+            <NavLink to='/comments' activeClassName='active'>Comments & Feedback</NavLink>
           </div>
-          <NavLink to='/comments'>Comments & Feedback</NavLink>
         </div>
     )
 }
