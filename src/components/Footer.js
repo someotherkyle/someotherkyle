66464e270862f1b3734a309f30e64b5505ad7b33
someotherkyle <kylemclellan@gmail.com>
2019-08-16 11:05:09 -0400

    basic, responsive design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,13 @@ export default class Footer extends Component {
   render(){
     return(
       <div className='row footer'>
-        <div className='col-xs-5 col-sm-5' />
-        <div className='col-xs-2 col-sm-2 footer-content'>
+        <div className='col-xs-3 col-sm-5' />
+        <div className='col-xs-6 col-sm-2 footer-content'>
           <a href="mailto:kyle@someotherkyle.com"><i className='material-icons'>email</i></a>
           <a rel='noopener noreferrer' target="_blank" href="https://github.com/someotherkyle"><img src="github.png" alt='github octocat' /></a>
           <a rel='noopener noreferrer' target="_blank" href="https://linkedin.com/in/someotherkyle"><img src="linkedin.png" alt='linked in logo' /></a>
         </div>
-        <div className='col-xs-5 col-sm-5' />
+        <div className='col-xs-3 col-sm-5' />
       </div>
     )
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,8 +6,8 @@ const NavBar = () => {
       <div className='navbar row'>
         <NavLink to='/' activeClassName='active'className='col-xs-12 col-sm-3'>About</NavLink>
         <NavLink to='/blog' activeClassName='active'className='col-xs-12 col-sm-3'>Blog</NavLink>
+        <NavLink to='/play2048' activeClassName='active' className='col-xs-12 col-sm-3'>Play 2048</NavLink>
         <NavLink to='/projects' activeClassName='active'className='col-xs-12 col-sm-3'>Projects</NavLink>
-        <NavLink to='/comments' activeClassName='active'className='col-xs-12 col-sm-3'>Comments & Feedback</NavLink>
       </div>
     )
 }
