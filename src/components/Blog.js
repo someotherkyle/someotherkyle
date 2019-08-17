05dfea8280a19953db7d6f714752180b16c66c4a
someotherkyle <kylemclellan@gmail.com>
2019-08-17 15:55:00 -0400

    blog component now sets innerhtml

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -6,6 +6,7 @@ class Blog extends Component {
   
   componentDidMount = () => {
     this.props.fetchPosts()
+    this.sleep(300).then(this.whichPost())
   }
 
   whichPost = () => {
@@ -15,16 +16,17 @@ class Blog extends Component {
     if (typeof(post) === 'undefined'){
       return (
         <div>
-          There was an issue with the redux store, please try refreshing. If that doesn't work please let me know by <a href="mailto: kyle@someotherkyle.com">email</a>. Sorry :(
+          There was an issue with the redux store or the post you are trying to reach does not exist. Please refresh or select the post from <a href="http://someotherkyle.com/blog">here</a>. If that doesn't work please let me know by <a href="mailto: kyle@someotherkyle.com">email</a>. Sorry :(
         </div>
       )
+    } else {
+      document.getElementById("blog-title").innerHTML = post.title 
+      document.getElementById("blog-body").innerHTML = post.content
     }
-    return (
-      <div className='blog-content'>
-        <h1>{post.title}</h1>
-        {post.content}
-      </div>
-    )
+  }
+
+  sleep = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms))
   }
 
   render(){
@@ -32,7 +34,8 @@ class Blog extends Component {
       <div className='row'>
         <div className='col-xs-1 col-sm-1' />
         <div className='col-xs-10 col-sm-10 blog'>
-          {this.whichPost()}
+          <h1 id="blog-title"> </h1>
+          <div id="blog-body"> </div>
         </div>
         <div className='col-xs-1 col-sm-1' />
       </div>
