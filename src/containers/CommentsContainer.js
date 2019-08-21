5da038d861f3f4b9af32f80c9863bb4fa51f510f
someotherkyle <kylemclellan@gmail.com>
2019-08-21 12:13:16 -0400

    fixed comments display immediately

--- src/containers/CommentsContainer.js
+++ src/containers/CommentsContainer.js
@@ -1,95 +1,128 @@
 import React, { Component } from 'react'
 import Comments from '../components/Comments'
-import { connect } from 'react-redux'
-import { setName, setContent, pushComment, fetchComments } from '../redux/actions/commentsActions'
 
-class CommentsContainer extends Component {
+export default class CommentsContainer extends Component {
   constructor(props){
     super(props)
 
     this.state = {
-      filter: '',
+      name: '',
+      content: '',
       postId: this.props.postId,
+      comments: [],
     }
   }
 
   componentDidMount(){
-    this.props.fetchComments()
+    this.fetchComments()
+  }
+
+  fetchComments = () => {
+    fetch("http://localhost:3001/comments")
+    .then(r => r.json())
+    .then(comments => this.setState({
+      ...this.state,
+      comments: comments.filter(comment => comment.post_id === this.state.postId),
+    }))
+  }
+
+  pushComment = () => {
+    fetch('http://localhost:3001/comments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        comment: {
+          name: this.state.name,
+          content: this.state.content,
+          post_id: this.state.postId
+        }
+      })
+    })
   }
 
   handleNameChange = e => {
     e.preventDefault()
-    this.props.setName(e.target.value)
+    this.setState({
+      ...this.state,
+      name: e.target.value
+    })
   }
 
   handleContentChange = e => {
     e.preventDefault()
-    this.props.setContent(e.target.value)
+    this.setState({
+      ...this.state,
+      content: e.target.value
+    })
   }
 
   handleSubmit = e => {
     e.preventDefault()
-    if (this.props.comments.name.length > 2 && this.props.comments.content.length > 3){
-      let commentInfo = {
-        name: this.props.comments.name,
-        content: this.props.comments.content,
+    if (this.state.name.length > 2 && this.state.content.length > 3){
+      const commentInfo = {
+        name: this.state.name,
+        content: this.state.content,
         postId: this.state.postId
       }
-      this.props.setName('')
-      this.props.setContent('')
-      this.props.pushComment(commentInfo)
-    }
-  }
-
-  handleFilterChange = e => {
-    e.preventDefault()
-    this.setState({
-      ...this.state,
-      filter: e.target.value
-    })
-  }
-
-  filterComments = () => {
-    let filteredComments = this.props.comments.all.filter( comment => comment.post_id === this.state.postId)
-    if (this.state.filter.length > 0){
-      let comments = filteredComments
-      filteredComments = []
-      comments.forEach( comment => {
-        if (comment.content.toLowerCase().includes(this.state.filter.toLowerCase())) filteredComments.push(comment)
+      this.setState({
+        ...this.state,
+        name: '',
+        content: '',
+        comments: [...this.state.comments, commentInfo]
       })
+      this.pushComment()
+      let form = document.getElementById('new-comment-form')
+      form.reset()
     }
-    return filteredComments
   }
+
+  // handleFilterChange = e => {
+  //   e.preventDefault()
+  //   this.setState({
+  //     ...this.state,
+  //     filter: e.target.value
+  //   })
+  //   this.filterComments()
+  // }
+
+  // filterComments = () => {
+  //   let filteredComments = []
+  //   if (this.state.filter.length > 0){
+  //     this.state.comments.forEach(comment => {
+  //       if (comment.content.toLowerCase().includes(this.state.filter.toLowerCase())) filteredComments.push(comment)
+  //     })
+  //     this.setState({
+  //       ...this.state,
+  //       filteredComments: filteredComments
+  //     })
+  //   } else {
+  //     this.setState({
+  //       ...this.state,
+  //       filteredComments: this.state.comments
+  //     })
+  //   }
+  // }
 
   render(){
     return(
       <div>
-        <div className='row new-comments-form'>
-          {this.props.comments.error !== '' ? <p>{this.props.comments.error} {this.props.comments.currentActivity}</p> : null}
-          <div className='col-sm-2' />
+        <div className='row new-comments'>
+          <div className='col-sm-4' />
           <div className='col-xs-12 col-sm-4'>
-            <form onSubmit={e => this.handleSubmit(e)}>
+            <form id="new-comment-form" onSubmit={e => this.handleSubmit(e)}>
               <input type='text' placeholder='Name' onChange={e => this.handleNameChange(e)} value={this.props.name} /><br />
               <textarea onChange={e => this.handleContentChange(e)} value={this.props.content} placeholder="Comment" /><br />
               <input type='submit' value='Submit' />
             </form>
           </div>
-          <div className='col-xs-12 col-sm-4'>
-            <label>Find:</label><br />
-            <input type="text" onChange={ e => this.handleFilterChange(e)} />
-          </div>
+          <div className='col-sm-4' />
         </div>
         <div className='row comments-wrapper'>
-          <Comments comments={this.filterComments()}/>
+          {this.state.comments.map(comment => <Comments name={comment.name} content={comment.content} created_at={comment.created_at} id={comment.id} key={comment.id} />)}
         </div>
       </div>
     )
   }
 }
-
-const mapStateToProps = state => {
-  return {
-    comments: state.comment
-  }
-}
-export default connect(mapStateToProps, { setName, setContent, pushComment, fetchComments })(CommentsContainer)
