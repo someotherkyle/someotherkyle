6ff635cde81eda1bcb3bf8108b6866b98e2b0cdc
someotherkyle <kylemclellan@gmail.com>
2019-08-27 17:01:35 -0400

    updated blog todo, added mobile support

--- src/containers/BoardContainer.js
+++ src/containers/BoardContainer.js
@@ -18,11 +18,13 @@ const eligibility = () =>{
 }
 
 class BoardContainer extends Component {
-  constructor(props){
+  constructor(props)  {
     super(props)
     this.state = {
       boardSide: 750
     }
+    this.startX = null
+    this.startY = null
   }
 
   handleKeyPress = e => {
@@ -63,22 +65,61 @@ class BoardContainer extends Component {
     this.props.toggleListener(false)
   }
 
+  handleTouchStart = e => {
+    e.preventDefault()
+    const t = e.touches[0]
+    console.log('start ' + t.screenX, t.screenY)
+
+    this.startX = t.screenX
+    this.startY = t.screenY
+  }
+
+  handleTouchMove = e => {
+    console.log(this.startX, this.startY)
+    e.preventDefault()
+    if (!this.startX || !this.startY) {
+      console.log('handleTouchMove Exiting early.')
+      return
+    }
+
+
+    const t = e.touches[0]
+    const endX = t.screenX
+    const endY = t.screenY
+
+    const diffX = this.startX - endX
+    const diffY = this.startY - endY
+    console.log('diff ' + diffX, diffY)
+    const minimumChange = 5
+    if (Math.abs(diffX) > minimumChange || Math.abs(diffY) > minimumChange) {
+      if (Math.abs(diffX) > Math.abs(diffY)) {
+        console.log('difference in X values ' + diffX)
+        diffX > 0 ? this.left() : this.right()
+      } else {
+        console.log('difference in Y values ' + diffY)
+        diffY > 0 ? this.up() : this.down()
+      }
+    }
+    this.startX = null
+    this.startY = null
+  }
+
   up = () => { 
     let board = this.props.game.board
     let score = this.props.game.score
     let eligible = eligibility()
-    if (!game.canMoveUp(board)){
+    if (!game.canMoveUp(board)) {
       return false
     }
-    for (let x = 0; x < 4; x++){
-      for (let y = 1; y < 4; y++){
-        if (board[x][y] !== 0){
-          for(let y1 = 0; y1 < y; y1++){
-            if (board[x][y1] === 0){
+    for (let x = 0; x < 4; x++) {
+      for (let y = 1; y < 4; y++) {
+        if (board[x][y] !== 0) {
+          for(let y1 = 0; y1 < y; y1++) {
+            if (board[x][y1] === 0) {
               board[x][y1] = board[x][y]
               board[x][y] = 0
               break
-            } else if (board[x][y] === board[x][y1] && game.clearShotVert(x, y1, y, board) && eligible[x][y1]){
+            } else if (board[x][y] === board[x][y1] && game.clearShotVert(x, y1, y, board) && eligible[x][y1]) {
               board[x][y1] += board[x][y]
               eligible[x][y1] = false
               score += board[x][y1]
@@ -96,18 +137,18 @@ class BoardContainer extends Component {
     let board = this.props.game.board
     let score = this.props.game.score
     let eligible = eligibility()
-    if (!game.canMoveDown(board)){
+    if (!game.canMoveDown(board)) {
       return false
     }
-    for (let x = 0; x < 4; x++){
-      for (let y = 2; y >= 0; y--){
-        if (board[x][y] !== 0){
-          for(let y1 = 3; y1 > y; y1--){
-            if (board[x][y1] === 0){
+    for (let x = 0; x < 4; x++) {
+      for (let y = 2; y >= 0; y--) {
+        if (board[x][y] !== 0) {
+          for(let y1 = 3; y1 > y; y1--) {
+            if (board[x][y1] === 0) {
               board[x][y1] = board[x][y]
               board[x][y] = 0
               break
-            } else if (board[x][y] === board[x][y1] && game.clearShotVert(x, y, y1, board) && eligible[x][y1]){
+            } else if (board[x][y] === board[x][y1] && game.clearShotVert(x, y, y1, board) && eligible[x][y1]) {
               board[x][y1] += board[x][y]
               eligible[x][y1] = false
               score += board[x][y1]
@@ -125,18 +166,18 @@ class BoardContainer extends Component {
     let board = this.props.game.board
     let score = this.props.game.score
     let eligible = eligibility()
-    if (!game.canMoveLeft(board)){
+    if (!game.canMoveLeft(board)) {
       return false
     }
-    for (let y = 0; y < 4; y++){
-      for (let x = 1; x < 4; x++){
-        if (board[x][y] !== 0){
-          for(let x1 = 0; x1 < x; x1++){
-            if (board[x1][y] === 0){
+    for (let y = 0; y < 4; y++) {
+      for (let x = 1; x < 4; x++) {
+        if (board[x][y] !== 0) {
+          for(let x1 = 0; x1 < x; x1++) {
+            if (board[x1][y] === 0) {
               board[x1][y] = board[x][y]
               board[x][y] = 0
               break
-            } else if (board[x][y] === board[x1][y] && game.clearShotHoriz(y, x1, x, board) && eligible[x1][y]){
+            } else if (board[x][y] === board[x1][y] && game.clearShotHoriz(y, x1, x, board) && eligible[x1][y]) {
               board[x1][y] += board[x][y]
               eligible[x1][y] = false
               score += board[x1][y]
@@ -154,18 +195,18 @@ class BoardContainer extends Component {
     let board = this.props.game.board
     let score = this.props.game.score
     let eligible = eligibility()
-    if (!game.canMoveRight(board)){
+    if (!game.canMoveRight(board)) {
       return false
     }
-    for (let y = 0; y < 4; y++){
-      for (let x = 2; x >= 0; x--){
-        if (board[x][y] !== 0){
-          for(let x1 = 3; x1 > x; x1--){
-            if (board[x1][y] === 0){
+    for (let y = 0; y < 4; y++) {
+      for (let x = 2; x >= 0; x--) {
+        if (board[x][y] !== 0) {
+          for(let x1 = 3; x1 > x; x1--) {
+            if (board[x1][y] === 0) {
               board[x1][y] = board[x][y]
               board[x][y] = 0
               break
-            } else if (board[x][y] === board[x1][y] && game.clearShotHoriz(y, x, x1, board) && eligible[x1][y]){
+            } else if (board[x][y] === board[x1][y] && game.clearShotHoriz(y, x, x1, board) && eligible[x1][y]) {
               board[x1][y] += board[x][y]
               eligible[x1][y] = false
               score += board[x1][y]
@@ -208,7 +249,7 @@ class BoardContainer extends Component {
     this.props.game.playerName === 'amu' ? board[tile.x][tile.y] = 4 : board[tile.x][tile.y] = tile.val
     this.props.updateBoard(board)
     this.props.updateScore(score)
-    if (game.noMove(board)){
+    if (game.noMove(board)) {
       canvas.drawGameOver(this.state.boardSide)
       const gameInfo = {
         score: this.props.game.score,
@@ -218,8 +259,8 @@ class BoardContainer extends Component {
     }
   } 
 
-  componentDidMount(){ //hard coded & will need to change if I implement various board sizes
-    if (this.props.game.ongoing){
+  componentDidMount() { //hard coded & will need to change if I implement various board sizes
+    if (this.props.game.ongoing) {
       window.addEventListener('keypress', event => {this.handleKeyPress(event)})
     } else {
       this.props.changePlayState()
@@ -236,9 +277,9 @@ class BoardContainer extends Component {
     return document.getElementById('board-div').offsetWidth
   }
 
-  componentDidUpdate(){
+  componentDidUpdate() {
     if (this.getBoardSide() >= 750) {
-      if (this.state.boardSide !== 750){
+      if (this.state.boardSide !== 750) {
         this.setState({ boardSide: 750 })
       }
     } else if (this.getBoardSide() !== this.state.boardSide) {
@@ -249,7 +290,7 @@ class BoardContainer extends Component {
     document.getElementById('board-div').style.height = this.state.boardSide.toString() + 'px' 
   }
   
-  render(){
+  render() {
     return(
       <div>
         <div className='board-wrapper'>
@@ -264,7 +305,7 @@ class BoardContainer extends Component {
             <div className='col-sm-2' />
           </div>
           <div className='row'>
-            <div id='board-div' onMouseOver={e => this.handleMouseOver()} onMouseOut={e => this.handleMouseOut()}>
+            <div id='board-div' onTouchStart={e => this.handleTouchStart(e)} onTouchMove={e => this.handleTouchMove(e)} onMouseOver={e => this.handleMouseOver()} onMouseOut={e => this.handleMouseOut()}>
               <Board side={this.state.boardSide} />
             </div>
           </div>
