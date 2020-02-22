4efc7b8820139c967200d46bb0410632fc34fe3f
someotherkyle <kyle@someotherkyle.com>
2020-02-22 12:37:10 -0500

    menu lives here now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom'
 const NavBar = () => {
     return(
       <div className='navbar'>
+        <span className='material-icons'>menu</span>
         <NavLink to='/' activeClassName='active' className=''>About</NavLink>
         <NavLink to='/blog' activeClassName='active' className=''>Blog</NavLink>
         <NavLink to='/play2048' activeClassName='active' className=''>Play 2048</NavLink>
