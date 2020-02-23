33c229bf62287e537b013244115d44d096398025
someotherkyle <kyle@someotherkyle.com>
2020-02-23 18:59:55 -0500

    mostly satisified with the landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,15 +17,19 @@ export default class Home extends Component {
           <div className='project secondary'>
             <h2><NavLink to="/blog">Blog</NavLink></h2>
 	    <p>A simple blog with comments connected to a <strong>Postgres</strong> database though <strong>Ruby on Rails</strong> and a <strong>React</strong> front end.</p>
+	    <a rel='noopener noreferrer' target='_blank' href='https://github.com/someotherkyle/someotherkyle'><i class="fas fa-code"></i></a>
           </div>
 
           <div className='project primary'>
             <h2><NavLink to="/play2048">2048 Tribute</NavLink></h2>
             <p>A tribute to the popular <a rel='noopener noreferrer' target='_blank' href="http://2048game.com">2048 game</a>. State is managed with <strong>Redux</strong> and high scores are persisted in the <strong>Postgres</strong> database.</p>
+	    <a rel='noopener noreferrer' target='_blank' href="https://github.com/someotherkyle/someotherkyle"><i class="fas fa-code"></i></a>
           </div>
 
           <div className='project secondary'>
-            <h2>Activity Tracker</h2>
+            <h2><a rel='noopener noreferrer' target='_blank' href='https://someotherkyle-activity-tracker.herokuapp.com'>Activity Tracker</a></h2>
+	    <p>My very first <strong>web application</strong>. It was built in <strong>Sinatra</strong> and was built with a <strong>SQLite</strong> database.</p>
+	    <a rel='noopener noreferrer' target='_blank' href="https://github.com/someotherkyle/activity-tracker"><i class="fas fa-code"></i></a>
           </div>
         </section>
       </div>
