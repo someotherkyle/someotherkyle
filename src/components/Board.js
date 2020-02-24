8b184acd15b9d516a73cf5f9c84b1a299c959e90
someotherkyle <kyle@someotherkyle.com>
2020-02-24 16:23:19 -0500

    minor styling tweaks, got board canvases aligned properly

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Board extends Component {
     render(){
         return(
-          <div id='board-div'>
+          <div>
             <canvas id='gameboard-canvas' height={this.props.side} width={this.props.side} />
             <canvas id='tile-canvas' height={this.props.side} width={this.props.side} />
             <canvas id='gameover-canvas' height={this.props.side} width={this.props.side} />
